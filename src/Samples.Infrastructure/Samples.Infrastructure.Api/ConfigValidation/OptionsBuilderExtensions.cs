fa8c97497297650b2f258f7df12982a834d7c480
Serhii Behma <veloceraptor89@gmail.com>
2024-09-06 18:29:20 +0300

    Add nested fields validation for options

---
+++ src/Samples.Infrastructure/Samples.Infrastructure.Api/ConfigValidation/OptionsBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Samples.Infrastructure.Api.ConfigValidation;
+
+public static class OptionsBuilderExtensions
+{
+    public static OptionsBuilder<TOptions> ValidateMiniValidation<TOptions>(this OptionsBuilder<TOptions> optionsBuilder)
+        where TOptions : class
+    {
+        // register the validator against the options
+        optionsBuilder.Services.TryAddSingleton<IValidateOptions<TOptions>>(
+            new MiniValidationValidateOptions<TOptions>(optionsBuilder.Name));
+        return optionsBuilder;
+    }
+}
---
+++ src/Samples.Infrastructure/Samples.Infrastructure.Api/ConfigValidation/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Samples.Infrastructure.Api.ConfigValidation;
+
+public static class ServiceCollectionExtensions
+{
+    public static TConfig AddConfigWithValidation<TConfig>(this IServiceCollection services, Action<TConfig> configureOptions)
+        where TConfig : class, new()
+    {
+        services.AddOptions<TConfig>()
+            .Configure(configureOptions)
+            .ValidateMiniValidation()
+            .ValidateOnStart();
+
+        var config = new TConfig();
+        configureOptions(config);
+
+        return config;
+    }
+
+    public static TConfig AddConfigWithValidation<TConfig>(this IServiceCollection services, IConfiguration configuration, string sectionName)
+        where TConfig : class, new()
+    {
+        services.AddOptions<TConfig>()
+            .Bind(configuration.GetSection(sectionName))
+            .ValidateMiniValidation()
+            .ValidateOnStart();
+
+        var config = configuration.GetSection(sectionName).Get<TConfig>() ?? new TConfig();
+
+        return config;
+    }
+
+    public static TConfig AddConfigWithValidation<TConfig>(this IServiceCollection services, IConfiguration configuration, string sectionName, Action<TConfig> configureOptions)
+        where TConfig : class, new()
+    {
+        services.AddOptions<TConfig>()
+            .Bind(configuration.GetSection(sectionName))
+            .Configure(configureOptions)
+            .ValidateMiniValidation()
+            .ValidateOnStart();
+
+        var config = configuration.GetSection(sectionName).Get<TConfig>() ?? new TConfig();
+        configureOptions(config);
+
+        return config;
+    }
+}
