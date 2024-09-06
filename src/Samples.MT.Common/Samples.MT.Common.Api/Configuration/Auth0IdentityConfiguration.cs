230b27ce813e47a4cfba694d69f1fade53af28cf
Serhii Behma <veloceraptor89@gmail.com>
2024-09-07 02:28:33 +0300

    Add OpenApi authentification

---
+++ src/Samples.MT.Common/Samples.MT.Common.Api/Configuration/Auth0IdentityConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Samples.MT.Common.Api.Configuration;
+
+public class Auth0IdentityConfiguration
+{
+    [Required] public string ClientId { get; set; } = string.Empty;
+    [Required] public string ClientSecret { get; set; } = string.Empty;
+    [Required] public string Authority { get; set; } = string.Empty;
+    [Required] public string Audience { get; set; } = string.Empty;
+}
