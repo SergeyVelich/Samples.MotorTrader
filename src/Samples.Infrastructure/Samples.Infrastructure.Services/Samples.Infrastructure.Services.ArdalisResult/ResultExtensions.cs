e9b8f46b093468d72bc90cf25687197d64476047
Serhii Behma <veloceraptor89@gmail.com>
2024-09-06 20:41:25 +0300

    Add result pattern (Ardalis)

---
+++ src/Samples.Infrastructure/Samples.Infrastructure.Services/Samples.Infrastructure.Services.ArdalisResult/ResultExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.Result;
+
+namespace Samples.Infrastructure.Services.ArdalisResult;
+
+public static class ResultExtensions
+{
+    public static Result ToResult<T>(this Result<T> result)
+    {
+        if (result.IsSuccess)
+        {
+            return Result.Success();
+        }
+
+        return result.Status switch
+        {
+            ResultStatus.Ok => Result.Success(),
+            ResultStatus.Created => Result.Success(),
+            ResultStatus.NoContent => Result.Success(),
+            ResultStatus.Conflict => Result.Conflict(result.Errors.ToArray()),
+            ResultStatus.Error => Result.Error(new ErrorList(result.Errors, result.CorrelationId)),
+            ResultStatus.Forbidden => Result.Forbidden(),
+            ResultStatus.Invalid => Result.Invalid(result.ValidationErrors),
+            ResultStatus.Unauthorized => Result.Unauthorized(),
+            ResultStatus.Unavailable => Result.Unavailable(result.Errors.ToArray()),
+            ResultStatus.CriticalError => Result.CriticalError(result.Errors.ToArray()),
+            ResultStatus.NotFound => Result.NotFound(result.Errors.ToArray()),
+            _ => throw new ArgumentException($"Unexpected result status type {result.Status}")
+        };
+    }
+}
---
+++ src/Samples.MT.Common/Samples.MT.Common.Api/Ardalis/MvcOptionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Result;
+using Ardalis.Result.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Samples.MT.Common.Api.Ardalis;
+
+public static class MvcOptionsExtensions
+{
+    public static MvcOptions ConfigureArdalis(this MvcOptions mvcOptions)
+    {
+        mvcOptions.AddResultConvention(resultStatusMap => resultStatusMap
+            .AddDefaultMap()
+            .For(ResultStatus.Ok, HttpStatusCode.OK, resultStatusOptions => resultStatusOptions
+                .For("POST", HttpStatusCode.Created)
+                .For("DELETE", HttpStatusCode.NoContent))
+            .For(ResultStatus.Error, HttpStatusCode.InternalServerError));
+
+        return mvcOptions;
+    }
+}
