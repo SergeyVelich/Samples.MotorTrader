e70942d105005ef3ac6bba372f6b83636aeebdc5
Serhii Behma <veloceraptor89@gmail.com>
2024-09-08 18:34:34 +0300

    Add Redis/memory cache

---
+++ src/Samples.MT.Common/Samples.MT.Common.Services/Samples.MT.Common.Services/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Samples.MT.Common.Services;
+
+public static class Constants
+{
+    public static class Cache
+    {
+        public const string TenantConfigurationsKey = "TenantConfigurations";
+    }
+}
